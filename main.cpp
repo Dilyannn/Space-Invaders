0b5a248158a5f6c135fe0d350b8b24ce07659373
kaloyan <milchevkaloian@gmail.com>
2025-05-15 12:16:22 +0300

    feat(raylib) Create raylib setup

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
-#include "header/Game.hpp"
+#include "raylib.h"
 
 int main() {
-    Game game;
-    game.run();
+    // 800x450 is 16:9
+    InitWindow(800, 450, "Raylib");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+
+        BeginDrawing();
+        ClearBackground(SKYBLUE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
+
+// #include "header/Game.hpp"
+//
+// int main() {
+//     Game game;
+//     game.run();
+//     return 0;
+// }
